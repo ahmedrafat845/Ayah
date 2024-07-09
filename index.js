04aab2ad96700281559c4ecd1960fdb64017b668
ahmedrafat845 <za399012@gmail.com>
2024-07-09 23:24:15 +0300

    ahmed last

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express'
 import { dbConnection } from './dataBase/dbConnection.js'
 import userRouter from './src/modules/users/user.router.js'
 import azkarRouter from './src/modules/azkar/azkar.router.js'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
 
+
+app.use(cors())
 app.use(express.json())
 app.use('/users',userRouter)
 app.use('/azkar',azkarRouter)
