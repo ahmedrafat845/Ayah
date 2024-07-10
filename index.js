050584fb9d51018215aa67b5d752b5fe5337c9c1
ahmedrafat845 <za399012@gmail.com>
2024-07-10 19:25:23 +0300

    ahadith

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import { dbConnection } from './dataBase/dbConnection.js'
 import userRouter from './src/modules/users/user.router.js'
 import azkarRouter from './src/modules/azkar/azkar.router.js'
+import hadithRouter from './src/modules/ahadith/ahadith.router.js';
 import cors from 'cors'
+
 const app = express()
 const port = 3000
 
@@ -12,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/users',userRouter)
 app.use('/azkar',azkarRouter)
+app.use('/ahadith',hadithRouter)
 
 
 dbConnection()
